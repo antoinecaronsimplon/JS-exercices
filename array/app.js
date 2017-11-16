231c916ea1f607e58725972c03d101059199a695
antoinecaron <antoinecaron.simplon@gmail.com>
2017-11-16 15:21:49 +0100

    tableau_exercice_1

--- array/app.js
+++ array/app.js
@@ -1,1 +1,6 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+
+// exercice 1
+var mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+
