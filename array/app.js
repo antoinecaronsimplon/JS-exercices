503f21c8c70e0c33b8b3895eb0bcf9894b5fac2c
antoinecaron <antoinecaron.simplon@gmail.com>
2017-11-16 16:04:39 +0100

    tableau_exercice_4

--- array/app.js
+++ array/app.js
@@ -2,10 +2,13 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 
 // exercice 1
-var mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+var mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
 
 // exercice 2
 console.log(mois[2]);
 
 // exercice 3
-console.log(mois[6]);
+console.log(mois[5]);
+
+//exercice 4
+console.log(mois[10]);
